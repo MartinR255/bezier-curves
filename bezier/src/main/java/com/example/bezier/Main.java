151759501ff751cb0bb47b6a3f7497bcfce7c4da
MartinR255 <ropjakm@gmail.com>
2023-07-29 20:55:31 +0200

    Add and delte node panel

--- bezier/src/main/java/com/example/bezier/Main.java
+++ bezier/src/main/java/com/example/bezier/Main.java
@@ -1,9 +1,12 @@
 package com.example.bezier;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
@@ -12,15 +15,24 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 public class Main extends Application {
     private Scene layoutScene;
     private Pane mainScene;
+    private HBox topPanel;
     private Stage stage;
     private BorderPane layout;
     private double width, height;
     private final double WIDTH_SCREEN_ADJUST_CONSTANT = 0.75, HEIGHT_SCREEN_ADJUST_CONSTANT = 0.7;
+
+    private enum Status {
+        ADD,
+        DRAG,
+        REMOVE
+    }
+    private Status currentState = Status.DRAG;
 
 
     @Override
@@ -30,20 +42,25 @@ public class Main extends Application {
         height = screenBounds.getHeight() * HEIGHT_SCREEN_ADJUST_CONSTANT;
 
         this.stage = stage;
-        mainScene = new Pane();
-
-
         // set up border pane layout
         layout = new BorderPane();
+
+        mainScene = new Pane();
         layout.setCenter(mainScene);
 
+        topPanel = new HBox();
+        layout.setTop(topPanel);
 
-        layoutScene = new Scene(layout, width, height);
-
+        initializeTopPanel();
         initializePoints();
         initializePointsCircles();
         setWindowEventListeners();
 
+
+
+        layoutScene = new Scene(layout, width, height);
+
+        paintScene();
         stage.setMinWidth(525);
         stage.setMinHeight(285);
         stage.setTitle("Bezier Curves");
@@ -61,7 +78,7 @@ public class Main extends Application {
     Points points;
     private void initializePoints() {
         points = new Points();
-        double part_size = width / 4;
+        double part_size = 200;
         for (int i = 0; i < DEFAULT_POINT_QUANTITY; i++) {
             points.addPoint(new Point(part_size * (i + 1), 350.0f));
         }
@@ -78,15 +95,15 @@ public class Main extends Application {
         List<List<Double>> midpoints = new ArrayList<>();
 
         while(points.size() > 1){
-		    final int num = points.size() - 1;
+            final int num = points.size() - 1;
             for(int i = 0; i < num; ++i){
                 a = points.get(i);
                 b = points.get(i+1);
                 midpoints.add(
-                    List.of(
-                            interpolate(a.get(0), b.get(0), t),
-                            interpolate(a.get(1), b.get(1), t)
-                    )
+                        List.of(
+                                interpolate(a.get(0), b.get(0), t),
+                                interpolate(a.get(1), b.get(1), t)
+                        )
                 );
             }
             points = midpoints;
@@ -100,7 +117,6 @@ public class Main extends Application {
 
     private final double delta = 0.05;
     private void drawCurve() {
-        System.out.println("idem idem");
         for (double t = 0; t <= 1.005; t += delta) {
             List<Double> p = deCasteljau(points.getPointsCoordinates(), t);
 
@@ -112,27 +128,27 @@ public class Main extends Application {
 
 
     List<Circle> pointCircleObjects;
+    private double pointRadius = 10;
     private void initializePointsCircles() {
         pointCircleObjects = new ArrayList<>();
         for (Point point : points.getPoints()) {
-            pointCircleObjects.add(new Circle(point.getX(), point.getY(), 10));
+            pointCircleObjects.add(new Circle(point.getX(), point.getY(), pointRadius));
         }
     }
 
 
-    private void paintScene() {
-        mainScene.getChildren().clear();
-        drawCurve();
-        mainScene.getChildren().addAll(pointCircleObjects);
-    }
-
-
-
     private void createDragController(int pointNumber) {
-        DragController dragController = new DragController(points.getPoint(pointNumber), pointCircleObjects.get(pointNumber));
+        DragController dragController = new DragController(points.getPoint(pointNumber), pointCircleObjects.get(pointNumber), topPanel.getPrefHeight() + pointRadius);
         dragControllers.add(dragController);
         dragController.objectChanged().addListener((observable, oldValue, newValue) -> paintScene());
     }
+
+
+    private void deletePoint(int index) {
+        pointCircleObjects.remove(index);
+        points.removePoint(index);
+    }
+
 
     List<DragController> dragControllers;
     private void setWindowEventListeners() {
@@ -141,16 +157,72 @@ public class Main extends Application {
             createDragController(i);
         }
 
-        stage.widthProperty().addListener((observable, oldValue, newValue) -> {
-            width = (double) newValue;
-            paintScene();
-        });
-
-        stage.heightProperty().addListener((observable, oldValue, newValue) -> {
-            height = (double) newValue;
-            paintScene();
+        mainScene.setOnMouseClicked(event -> {
+            if (currentState == Status.ADD) {
+                Point point = new Point(event.getX(), event.getY());
+                points.addPoint(point);
+                pointCircleObjects.add(new Circle(point.getX(), point.getY(), 10));
+                createDragController(pointCircleObjects.size()-1);
+                paintScene();
+            } else if (currentState == Status.REMOVE) {
+                for (int i=0; i < points.getPointsSize(); i++) {
+                    Point point = points.getPoint(i);
+                    double xDiff = point.getX() - event.getX();
+                    double yDiff = point.getY() - event.getY();
+                    if ((xDiff * xDiff) + (yDiff * yDiff) <= pointRadius * pointRadius) {
+                        deletePoint(i);
+                        paintScene();
+                        break;
+                    }
+                }
+            }
         });
     }
+
+    public void paintScene() {
+        mainScene.getChildren().clear();
+        drawCurve();
+        mainScene.getChildren().addAll(pointCircleObjects);
+    }
+
+    public void initializeTopPanel() {
+        topPanel.getStylesheets().add(
+                Objects.requireNonNull(this.getClass().getResource("/css/top_panel_style.css")).toExternalForm());
+        topPanel.getStyleClass().add("hbox");
+        topPanel.setAlignment(Pos.CENTER_LEFT);
+        topPanel.setPrefHeight(30);
+        topPanel.setSpacing(20);
+        setupButtons();
+    }
+
+
+    private void setupButtons() {
+        Button addNewNode = new Button("+");
+        Button deleteNode = new Button("-");
+
+        addNewNode.setOnAction(event -> {
+            if (currentState == Status.DRAG) {
+                currentState = Status.ADD;
+            } else if (currentState == Status.REMOVE) {
+                currentState = Status.ADD;
+            } else {
+                currentState = Status.DRAG;
+            }
+        });
+
+        deleteNode.setOnAction(event -> {
+            if (currentState == Status.DRAG) {
+                currentState = Status.REMOVE;
+            } else if (currentState == Status.ADD) {
+                currentState = Status.REMOVE;
+            } else {
+                currentState = Status.DRAG;
+            }
+        });
+
+        topPanel.getChildren().addAll(addNewNode, deleteNode);
+    }
+
 }
 
 
